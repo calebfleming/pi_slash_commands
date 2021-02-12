d90831deb2ad866495aa0f21ba2ae84f690ca898
calebfleming <31708395+calebfleming@users.noreply.github.com>
2021-02-12 16:19:21 +0100

    body movin body parsin

--- index.js
+++ index.js
@@ -1,33 +1,32 @@
 // Import express and request modules
 var express = require('express');
 var request = require('axios');
+var bodyParser = require('body-parser');
 
 // Store our app's ID and Secret. These we got from Step 1.
-// For this tutorial, we'll keep your API credentials right here. But for an actual app, you'll want to  store them securely in environment variables.
 var clientId = 'clientId';
 var clientSecret = 'clientSecret';
 
 const gifConfig = {
   gotem: 'https://media.giphy.com/media/Ko7UAfIKrlTyw/giphy.gif',
   doc: 'https://media.giphy.com/media/4C68wmly8RyUg/giphy.gif',
-  strut: 'https://media.giphy.com/media/ghpdI3dNfX0pW/giphy.gif',
+  getsome: 'https://media.giphy.com/media/ghpdI3dNfX0pW/giphy.gif',
   yeahyep: 'https://media.giphy.com/media/3oFzm7MaLnMdD1T6tG/giphy.gif',
   interesting: 'https://media.giphy.com/media/VFYJXIuuFl6pO/giphy.gif',
   lolz: 'https://media.giphy.com/media/VIVWFx6c91AAwWLwWB/giphy.gif',
   nope: 'https://media.giphy.com/media/fDO2Nk0ImzvvW/giphy.gif',
   undertaker: 'https://media.giphy.com/media/b6iVj3IM54Abm/giphy.gif',
+  why: 'https://media.giphy.com/media/3o7btYetccbRYL4WVW/giphy.gif',
+  smiling: 'https://media.giphy.com/media/xghFgmOKbk0G4/giphy.gif'
 };
 
-
-// Instantiates Express and assigns our app variable to it
 var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-// Again, we define a port we want to listen to
-const PORT=8888;
+const PORT=6969;
 
-// Lets start our server
 app.listen(PORT, function () {
-    //Callback triggered when server is successfully listening. Hurray!
     console.log("Example app listening on port " + PORT);
 });
 
@@ -38,19 +37,15 @@ app.get('/', function(req, res) {
 
 // This route handles get request to a /oauth endpoint. We'll use this endpoint for handling the logic of the Slack oAuth process behind our app.
 app.get('/oauth', function(req, res) {
-    // When a user authorizes an app, a code query parameter is passed on the oAuth endpoint. If that code is not there, we respond with an error message
     if (!req.query.code) {
         res.status(500);
         res.send({"Error": "Looks like we're not getting code."});
         console.log("Looks like we're not getting code.");
     } else {
-        // If it's there...
-        // We'll do a GET call to Slack's `oauth.access` endpoint, passing our app's client ID, client secret, and the code we just got as query parameters.
         request({
             url: 'https://slack.com/api/oauth.access', //URL to hit
             qs: {code: req.query.code, client_id: clientId, client_secret: clientSecret}, //Query string data
-            method: 'GET', //Specify the method
-
+            method: 'GET',
         }, function (error, response, body) {
             if (error) {
                 console.log(error);
@@ -62,45 +57,24 @@ app.get('/oauth', function(req, res) {
     }
 });
 
-app.post('/command_generic', function(req, res) {
-  const requestParam = req.text || 'invalid';
+app.post('/barnwood', function(req, res) {
+  console.log(req)
+  const requestParam = req.body.text || 'invalid';
 
   const getRandom = () => {
-    const properties = Object.getOwnPropertyNames(gifConfig);
-    const index = Math.floor(Math.random() * properties.length);
-    return gifConfig[properties[index]];
+  const properties = Object.getOwnPropertyNames(gifConfig);
+  const index = Math.floor(Math.random() * properties.length);
+  return gifConfig[properties[index]];
   }
 
   const response = {
     response_type: 'in_channel',
     attachments: [
       {
-        image_url = gifConfig[requestParam]  ? gifConfig[requestParam] : getRandom()
+	      image_url : gifConfig[requestParam]  ? gifConfig[requestParam] : getRandom()
       }
     ]
   };
 
   res.json(response);
-});
-
-app.post('/command_gotem', function(req, res) {
-let gotem_data = {
-  response_type: 'in_channel',
-  attachments:[
-    {
-      image_url: 'https://media.giphy.com/media/Ko7UAfIKrlTyw/giphy.gif'
-    }
-]};
-res.json(gotem_data);
-});
-
-app.post('/command_doc', function(req, res) {
-	let doc_data = {
-		response_type: 'in_channel',
-		attachments : [
-			{
-				image_url : 'https://media.giphy.com/media/4C68wmly8RyUg/giphy.gif'
-			}
-		]};
-	res.json(doc_data);
 });
